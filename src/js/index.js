b9d9749a2068c0d3777f24786187e7adbf5dc3dd
kinoshita0623 <kuutarousii@gmail.com>
2020-09-30 21:07:50 +0900

    要素のレンダー

--- src/js/index.js
+++ src/js/index.js
@@ -27,5 +27,22 @@ class App extends React.Component{
     }
 }
 
+
+
+function tick(){
+    const element = (
+        <div>
+            <h1>Hello world</h1>
+            <h2>It is { new Date().toLocaleTimeString() }</h2>
+        </div>
+        
+    );
+
+    ReactDOM.render(element, document.getElementById("root"));
+
+}
+
+setInterval(tick, 1000);
+
 const app = document.getElementById('app');
 ReactDOM.render(<App/>, app);
